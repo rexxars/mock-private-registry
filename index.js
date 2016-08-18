f3bd9559652c7cba831897d97ff31247c0befc7a
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-08-18 13:23:00 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,61 @@
+var fs = require('fs')
+var path = require('path')
+var http = require('http')
+var responseTemplate = require('./responseTemplate.json')
+
+var tarballPath = path.join(__dirname, 'mock.tgz')
+
+module.exports = function(opts, cb) {
+  var callback = typeof opts === 'function' ? opts : cb
+  var options = typeof opts === 'function' ? {} : opts
+
+  var hostname = options.hostname || '127.0.0.1'
+  var port = options.port || 63142
+  var token = options.token || 'MySecretToken'
+
+  var server = http.createServer(function (req, res) {
+    if (req.method !== 'GET') {
+      res.statusCode = 405
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({error: 'Method Not Allowed'}))
+      return
+    }
+
+    var authToken = (req.headers.authorization || '').split(' ', 2)
+    if (authToken[0] !== 'Bearer' || authToken[1] !== token) {
+      res.statusCode = 403
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({error: 'Incorrect or missing token'}))
+      return
+    }
+
+    if (req.url === '/@mockscope%2Ffoobar') {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify(generatePackageResponse(hostname, port)))
+      return
+    }
+
+    if (req.url === '/@mockscope/foobar/-/foobar-1.0.0.tgz') {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/octet-stream')
+      fs.createReadStream(tarballPath).pipe(res)
+      return
+    }
+
+    res.statusCode = 404
+    res.end(JSON.stringify({error: 'File Not Found'}))
+  })
+
+  server.listen(port, hostname, function () {
+    callback(null, server)
+  })
+}
+
+function generatePackageResponse(hostname, port) {
+  var tpl = Object.assign({}, responseTemplate)
+  tpl.versions['1.0.0'].dist = Object.assign({}, tpl.versions['1.0.0'].dist, {
+    tarball: 'http://' + hostname + ':' + port + '/@mockscope/foobar/-/foobar-1.0.0.tgz'
+  })
+  return tpl
+}
---
+++ test/registry.test.js
@@ -0,0 +1,96 @@
+var test = require('tape')
+var got = require('got')
+var crypto = require('crypto')
+var mock = require('../')
+var responseTemplate = require('../responseTemplate.json')
+
+var token = 'MySecretToken'
+var defaultHeaders = {'Authorization': 'Bearer ' + token}
+var req = function (path, opts) {
+  return got(
+    /https?:\/\//.test(path) ? path : 'http://localhost:63142' + path,
+    Object.assign({timeout: 150, headers: defaultHeaders}, opts || {})
+  )
+}
+
+var server
+test('boot', function (t) {
+  mock(function(err, httpServer) {
+    if (err) {
+      return t.fail(err)
+    }
+
+    server = httpServer
+    t.end()
+  })
+})
+
+test('403s without a token', function (t) {
+  req('/meh', {headers: {}}).then(function (res) {
+    t.fail('Did not 403 as expected')
+  }).catch(function (err) {
+    t.equal(err.statusCode, 403)
+  }).then(t.end)
+})
+
+test('405s on non-GETs', function (t) {
+  req('/@mockscope%2Ffoobar', {method: 'POST'}).then(function (res) {
+    t.fail('Did not 405 as expected')
+  }).catch(function (err) {
+    t.equal(err.statusCode, 405)
+  }).then(t.end)
+})
+
+test('404s on unknown URLs', function (t) {
+  req('/blahtti').then(function (res) {
+    t.fail('Did not 404 as expected')
+  }).catch(function (err) {
+    t.equal(err.statusCode, 404)
+  }).then(t.end)
+})
+
+test('returns package response on valid package request', function (t) {
+  req('/@mockscope%2Ffoobar', {json: true}).then(function (res) {
+    var body = res.body
+    var shasum = (
+      body && body.versions && body.versions['1.0.0'] &&
+      body.versions['1.0.0'].dist && body.versions['1.0.0'].dist.shasum
+    )
+
+    t.equal(shasum, responseTemplate.versions['1.0.0'].dist.shasum)
+  }).catch(function (err) {
+    t.fail('Did not 200 as expected (' + err.message + ')')
+  }).then(t.end)
+})
+
+test('returns tarball on valid tarball request', function (t) {
+  req('/@mockscope/foobar/-/foobar-1.0.0.tgz', {encoding: null}).then(function (res) {
+    var shasum = crypto.createHash('sha1').update(res.body).digest('hex')
+    t.equal(shasum, responseTemplate.versions['1.0.0'].dist.shasum)
+  }).catch(function (err) {
+    t.fail('Did not 200 as expected (' + err.message + ')')
+  }).then(t.end)
+})
+
+test('returned tarball url is downloadable', function (t) {
+  req('/@mockscope%2Ffoobar', {json: true}).then(function (res) {
+    var body = res.body
+    var tarball = (
+      body && body.versions && body.versions['1.0.0'] &&
+      body.versions['1.0.0'].dist && body.versions['1.0.0'].dist.tarball
+    )
+
+    req(tarball, {encoding: null}).then(function (tarRes) {
+      var shasum = crypto.createHash('sha1').update(tarRes.body).digest('hex')
+      t.equal(shasum, responseTemplate.versions['1.0.0'].dist.shasum)
+    }).catch(function (err) {
+      t.fail('Tarball request failed: ' + err.message)
+    }).then(t.end)
+  }).catch(function (err) {
+    t.fail('Did not 200 as expected (' + err.message + ')')
+  })
+})
+
+test('shutdown', function (t) {
+  server.close(t.end)
+})
